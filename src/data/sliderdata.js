2d1a8d597006f1958a5506a135bead7b58402a9f
BOSS LADY <longlaoneilla@gmail.com>
2022-08-30 13:16:39 +0100

    Added new folders, data and components. deleted some unnecessary folders

---
+++ src/data/sliderdata.js
@@ -0,0 +1,56 @@
+const sliderdata = {
+    slides:[
+        {
+            id:1,
+            label:'Gaming in the Metaverse',
+            title:'Media and Entertainment',
+            caption:'How gamers in the metaverse are changing the rules of success',
+            image:'/images/sl1.jpg'
+        },
+        {
+            id:2,
+            label:'Workers and the great Resignation',
+            title:'Leadership and Talent',
+            caption:'Quitting or Refitting? Solving the Great Resignation',
+            image:'/images/sl2.jpg'
+        },
+        {
+            id:3,
+            label:'Private Equity Midyear Report',
+            title:'Private Equity Midyear Report',
+            caption:'After a strong first half, a potential slowdown looms',
+            image:'/images/sl4.jpg'
+        },
+        {
+            id:4,
+            label:'Recession Playbook',
+            title:'The New Recession Playbook',
+            caption:'How the best businesses plan for a downturn',
+            image:'/images/sl3.jpeg'
+        }
+        
+    ],
+    industries:[
+        {
+            id:1,
+            title:'Retail'
+        },
+        {
+            id:2,
+            title:'Technology'
+        },
+        {
+            id:3,
+            title:'Finance'
+        },
+        {
+            id:4,
+            title:'Health Care'
+        },
+        {
+            id:5,
+            title:'Oil & Gas'
+        }
+    ]
+}
+export default sliderdata;
